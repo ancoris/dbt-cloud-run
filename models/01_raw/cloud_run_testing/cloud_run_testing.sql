4442caf5fecc06b535b502f74723334ad4128256
Bailey Sharp-Ledger <61457228+bsharp-ledger@users.noreply.github.com>
2023-09-18 14:56:16 +0100

    Update cloud_run_testing.sql

--- models/01_raw/cloud_run_testing/cloud_run_testing.sql
+++ models/01_raw/cloud_run_testing/cloud_run_testing.sql
@@ -12,7 +12,7 @@ select
   {% if is_incremental() %}
     (select max(id) from {{ this }}) +
   {% endif %}                
-  1                       as id, 
+  1/0                       as id, 
 
   session_user()          as session_user,
   current_timestamp()     as current_timestamp,
