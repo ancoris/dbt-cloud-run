281e3de689ffff15a8bb1c8eacdf7e8e4dbc2354
Bailey Sharp-Ledger <61457228+bsharp-ledger@users.noreply.github.com>
2023-09-22 16:16:29 +0100

    Update upload_artefacts.sh

--- .docker/upload_artefacts.sh
+++ .docker/upload_artefacts.sh
@@ -4,11 +4,11 @@ date=$(date +'%Y-%m-%dT%H:%M:%S')
 #GCS_ARTIFACTS_BUCKET="data-sandbox-266217-misc-data" # delete before uploading
 
 # graph.gpickle
-gsutil cp target/graph.gpickle gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/latest/graph.gpickle
-gsutil cp target/graph.gpickle gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/$date/graph.gpickle
+gcloud storage cp target/graph.gpickle gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/latest/graph.gpickle
+gcloud storage cp target/graph.gpickle gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/$date/graph.gpickle
 echo "graph.gpickle uploaded successfully\n"
 
 # manifest
-gsutil cp target/manifest.json gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/latest/manifest.json
-gsutil cp target/manifest.json gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/$date/manifest.json
+gcloud storage cp target/manifest.json gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/latest/manifest.json
+gcloud storage cp target/manifest.json gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/$date/manifest.json
 echo "manifest.json uploaded successfully\n"
