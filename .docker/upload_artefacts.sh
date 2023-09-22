3a99113ca0b1515e47d0ebc2c0940af3bad8d6bb
Bailey Sharp-Ledger <61457228+bsharp-ledger@users.noreply.github.com>
2023-09-22 16:32:39 +0100

    debugging upload_artefacts

--- .docker/upload_artefacts.sh
+++ .docker/upload_artefacts.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# debugging
 date=$(date +'%Y-%m-%dT%H:%M:%S')
-#GCS_ARTIFACTS_BUCKET="data-sandbox-266217-misc-data" # delete before uploading
+GCS_ARTIFACTS_BUCKET="data-sandbox-266217-misc-data" # delete before uploading
+CLOUD_RUN_JOB="dbt-job3"
+
+echo "date="$date
+echo "GCS_ARTIFACTS_BUCKET="$GCS_ARTIFACTS_BUCKET
+echo "CLOUD_RUN_JOB="$CLOUD_RUN_JOB
 
 # graph.gpickle
 gcloud storage cp target/graph.gpickle gs://$GCS_ARTIFACTS_BUCKET/dbt_cloud_run/$CLOUD_RUN_JOB/latest/graph.gpickle
